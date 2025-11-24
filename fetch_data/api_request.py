894ae8b25bf18fd3674106b2c78fb0007d3e6281
Suraj <abodunrinsurajudeen@gmail.com>
2025-11-24 21:30:45 +0100

    api_request and docker-compose setup

---
+++ fetch_data/api_request.py
@@ -0,0 +1,31 @@
+import requests as req
+import os
+from  dotenv import load_dotenv
+
+##load environment variable
+load_dotenv()
+
+api_key = os.getenv('api_key')
+
+
+api_url = f"https://api.weatherstack.com/current?access_key={api_key}&query=New York"
+
+
+def fetch_data() ->None:
+    try:
+        print('Fetching weather data from the WeatherStack API website')
+
+        response = req.get(api_url)
+        response.raise_for_status()
+        print('Api response received successfully')
+        return response.json()
+    
+    except req.exceptions.RequestException as e:
+        print(f'An error occured {e}')
+        raise
+
+
+if __name__=='__main__':
+    fetch_data()
+
+
